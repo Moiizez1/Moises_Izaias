c2712a553123fcc1db4e0ac14f1435ea0fcd29f7
Moiizez1 <57538862+Moiizez1@users.noreply.github.com>
2019-11-25 13:59:01 -0300

    juntar

--- fase02/ex06/juntar.js
+++ fase02/ex06/juntar.js
@@ -1,7 +1,5 @@
 function juntar (num1, num2){
-    num1.toString();
-    num2.toString();
-    parseInt(num1);
-    parseInt(num2);
+    Number(num1);
+    Number(num2);
     return num1+""+num2;
 }
