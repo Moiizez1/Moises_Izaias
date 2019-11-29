01d9eee1a265172708439f03add0c5fc57d62424
Moiizez1 <57538862+Moiizez1@users.noreply.github.com>
2019-11-29 16:37:30 -0300

    Vogal ou Consoante

--- fase03/ex03/vogalOuConsoante.js
+++ fase03/ex03/vogalOuConsoante.js
@@ -1,7 +1,7 @@
 function vogalOuConsoante(letra){
 
 
-if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
+if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'||letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
     return "Vogal";
 }
 
